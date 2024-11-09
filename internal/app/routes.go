c378f6e30760f8b09fd0d9b5d84c52507b12d3cc
Elliott Minns <elliott.minns@pm.me>
2024-11-08 22:14:03 -0600

    remove middleware for rate limiting

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -3,26 +3,18 @@ package app
 import (
 	"html/template"
 	"net/http"
-	"time"
 
 	"github.com/dreamsofcode-io/guestbook/internal/handler"
-	"github.com/dreamsofcode-io/guestbook/internal/middleware"
 )
 
 func (a *App) loadRoutes(tmpl *template.Template) {
 	guestbook := handler.New(a.logger, a.db, tmpl)
-	ratelimiter := middleware.RateLimiter{
-		Period:  time.Minute,
-		MaxRate: 2,
-		Store:   a.rdb,
-	}
 
 	files := http.FileServer(http.Dir("./static"))
+
 	a.router.Handle("GET /static/", http.StripPrefix("/static", files))
 
 	a.router.Handle("GET /{$}", http.HandlerFunc(guestbook.Home))
 
-	a.router.Handle("POST /{$}", ratelimiter.Middleware(
-		http.HandlerFunc(guestbook.Create),
-	))
+	a.router.Handle("POST /{$}", http.HandlerFunc(guestbook.Create))
 }
